76afa11e52ce85ca35b3159fe44145cd35253291
Erwin Wester <erwin.wester@ict-bp.nl>
2022-03-25 14:59:36 +0100

    Initial audio files added

---
+++ Assets/_Project/_Scripts/Managers/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spellenbakkerij 
+{
+    public class MainMenu : MonoBehaviour
+    {
+		AudioSystem audioSystem; 
+		private void OnEnable() {
+			
+		}
+	}
+}
