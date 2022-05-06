0af5d5d5d2f8f4dc588b6893430269af1a50f982
Erwin Wester <erwin.wester@ict-bp.nl>
2022-05-06 14:41:10 +0200

    Schedule clips using corouitines Introduced Music Set with intro and loop section

--- Assets/_Project/_Scripts/Models/MusicSet.cs
+++ Assets/_Project/_Scripts/Models/MusicSet.cs
@@ -1,13 +1,16 @@
 ﻿namespace Spellenbakkerij {
 	using System;
+	using System.Collections;
 
 	using UnityEngine;
 
 	[Serializable]
 	public class MusicSet {
-		public Clip[] clips;
-		public float fadein;
-		public float fadeout;
+		public Clip[] Intro;
+		public Clip[] Loop;
+		public float FadeIn;
+		public float FadeOut;
+		public float FadeOverlap;
 
 		public int Index { get; set; }
 	}
@@ -15,12 +18,11 @@
 	[Serializable]
 	public class Clip {
 		public AudioClip audioClip;
-		public bool loop;
 		/// <summary>
 		/// loop and repeat are mutually exclusive. If loop then no repeat. If repeat then no loop.
 		/// loop true/false is leading in this.
 		/// </summary>
-		public int repeat;
+		public int Iterations = 1;
 
 		public double Duration => (double) audioClip.samples / audioClip.frequency;
 	}
--- Assets/_Project/_Scripts/Systems/AudioSystem.cs
+++ Assets/_Project/_Scripts/Systems/AudioSystem.cs
@@ -1,6 +1,9 @@
 namespace Spellenbakkerij {
 	using System;
 	using System.Collections;
+	using System.Collections.Concurrent;
+	using System.Collections.Generic;
+	using System.Linq;
 
 	using UnityEngine;
 
@@ -9,96 +12,131 @@ namespace Spellenbakkerij {
 	/// Ensure you change the 'Sounds' audio source to use 3D spatial blend if you intend to use 3D sounds.
 	/// </summary>
 	public class AudioSystem : StaticInstance<AudioSystem> {
-		[SerializeField] private AudioSource _soundsSource;
+		private AudioSource _soundsSource;
+		private AudioSource _activeMusicSource;
+		private AudioSource _inactiveMusicSource;
 
-		private AudioSource[] _musicSource;
-		private int _musicSourceIndex;
-
-		/// <summary>
-		/// This is an internal pointer to the exact (Double) moment that the current Clip will stop playing
-		/// </summary>
-		private double _musicCurrentPlayingEndTime;
-		private (AudioSource MusicSource, double StartFade, float FadeTime, bool Active) _musicFadeOut;
-		private (AudioSource MusicSource, double StartFade, float FadeTime, bool Active) _musicFadeIn;
-
-		private const float MusicPrepartationDelay = 0.5f;
+		private const float MusicPreparationDelay = 0.6f;
+		private const float ScheduleAheadDuration = 1f;
 		private MusicSet _musicSet;
+		private MusicSet _nextMusicSet;
 
-		public bool IsPlaying { get; private set; }
+		private double _clipEndTime = 0;
+		private double _scheduleEndTime = 0;
+		private readonly ConcurrentDictionary<double, Coroutine> _scheduledCoroutines = new ConcurrentDictionary<double, Coroutine>();
+
+		private Clip _lastScheduledClip = null;
+		private int _lastScheduledClipIndex = 0;
+		private MusicSetPart _lastScheduledMusicSetPart = MusicSetPart.Intro;
+		private int _lastScheduledPlayIteration = 0;
+
+		public bool IsPlaying { get; private set; } = false;
 
 		override protected void Awake() {
-			this._musicSource = new AudioSource[2];
-			this._musicSource[0] = this.gameObject.AddComponent<AudioSource>();
-			this._musicSource[1] = this.gameObject.AddComponent<AudioSource>();
-			this._musicSourceIndex = 0;
-			this._musicCurrentPlayingEndTime = 0d; // initial value
-			this._musicFadeOut = (null, 0, 0, false);
-			this._musicFadeIn = (null, 0, 0, false);
-			this.IsPlaying = false;
+			this._soundsSource = this.gameObject.AddComponent<AudioSource>();
+			this._activeMusicSource = this.gameObject.AddComponent<AudioSource>();
+			this._inactiveMusicSource = this.gameObject.AddComponent<AudioSource>();
+			Debug.Assert(ScheduleAheadDuration > MusicPreparationDelay, "Plantijd moet groter zijn dan de voorbereidingstijd");
+
+			this._scheduleEndTime = AudioSettings.dspTime;
+
 			base.Awake();
 		}
 
 		internal void PlayMusic(MusicSet musicSet) {
-			if (this.IsPlaying) {
-				// if previous MusicSet is playing, fadeout old and fadein new
-				// longest of both fadein and fadeout time
-				float longestTime = Math.Max(this._musicSet.fadeout, musicSet.fadein);
-				// cross over time
-				double crossOverTime = AudioSettings.dspTime + Math.Max(longestTime, MusicPrepartationDelay);
-
-				// set fadeout
-				this._musicFadeOut = (this.ActiveMusicSource, crossOverTime - this._musicSet.fadeout, this._musicSet.fadeout, false);
-				// set fadein
-				this._musicFadeIn = (this.OtherMusicSource, crossOverTime - musicSet.fadein, musicSet.fadein, false);
-
-				this._musicCurrentPlayingEndTime = crossOverTime;
-			} else {
-				// if not, just fadein new
-				// set fadein
-				this._musicFadeIn = (this.OtherMusicSource, AudioSettings.dspTime + MusicPrepartationDelay, musicSet.fadein, false);
-
-				this._musicCurrentPlayingEndTime = AudioSettings.dspTime + Math.Max(musicSet.fadein, MusicPrepartationDelay);
-			}
-
-			// Replace current music set with the new one
-			// TODO: This could be a problem if the currently playing music set is replaced?
-			this._musicSet = musicSet;
-			this._musicSet.Index = 0;
+			// Schedule the next music set.
+			this._nextMusicSet = musicSet;
 			this.IsPlaying = true;
-			// set endtime
 		}
 
-		private void FixedUpdate() {
-			// time to schedule next music clip
-			// It does this MusicPrepartationDelay before the end time of the currently playing clip
-			if (this._musicSet.Index < this._musicSet.clips.Length && AudioSettings.dspTime > this._musicCurrentPlayingEndTime - MusicPrepartationDelay) {
-				Clip clip = this._musicSet.clips[this._musicSet.Index++];
-				Debug.Log($"AudioSystem.Update(): {AudioSettings.dspTime} > {this.OtherMusicSource.GetInstanceID()} > Enqueue {clip.audioClip.name} at {this._musicCurrentPlayingEndTime}");
-				EnqueueMusic(clip);
+		private void Update() {
+			if (!this.IsPlaying) { return; }
+
+			void SetNextMusicSet() {
+				this._musicSet = this._nextMusicSet;
+				this._nextMusicSet = null;
+				this._lastScheduledMusicSetPart = MusicSetPart.Intro;
 			}
 
-			// time to start fadeout
-			if (!this._musicFadeOut.Active && this._musicFadeOut.StartFade > 0 && AudioSettings.dspTime > this._musicFadeOut.StartFade) {
-				this._musicFadeOut.Active = true;
-				Debug.Log($"AudioSystem.Update(): {AudioSettings.dspTime} > {this._musicFadeOut.MusicSource.GetInstanceID()} > Fadeout at {this._musicFadeOut.StartFade} length {this._musicFadeOut.FadeTime}");
-				_ = this.StartCoroutine(FadeOutMusic(this._musicFadeOut.MusicSource, this._musicFadeOut.FadeTime));
+			if (this._musicSet == null) {
+				if (this._nextMusicSet == null) {
+					return;
+				} else {
+					SetNextMusicSet();
+				}
 			}
 
-			// time to start fadein
-			if (!this._musicFadeIn.Active && this._musicFadeIn.StartFade > 0 && AudioSettings.dspTime > this._musicFadeIn.StartFade) {
-				this._musicFadeIn.Active = true;
-				Debug.Log($"AudioSystem.Update(): {AudioSettings.dspTime} > {this._musicFadeIn.MusicSource.GetInstanceID()} > Fadein at {this._musicFadeIn.StartFade} length {this._musicFadeIn.FadeTime}");
-				_ = this.StartCoroutine(FadeInMusic(this._musicFadeIn.MusicSource, this._musicFadeIn.FadeTime));
+			// Check of er een fade-out/in gedaan moet worden.
+			if (this._nextMusicSet != null) {
+				foreach (Coroutine coroutine in this._scheduledCoroutines.Values) {
+					this.StopCoroutine(coroutine);
+				}
+				this._scheduledCoroutines.Clear();
+
+				double fadeOutTime = this._musicSet.FadeOut;
+				double fadeOverlapTime = this._musicSet.FadeOverlap;
+				double fadeInTime = this._nextMusicSet.FadeIn;
+
+				Debug.Assert(fadeOutTime <= MusicPreparationDelay, "Fade out time is larger than preparation delay.");
+				Debug.Assert(fadeOverlapTime <= MusicPreparationDelay, "Fade overlap time is larger than preparation delay.");
+				Debug.Assert(fadeInTime <= MusicPreparationDelay, "Fade in time is larger than preparation delay.");
+				Debug.Assert(fadeOverlapTime <= fadeOutTime, "Fade overlap time is larger than fade out time.");
+				Debug.Assert(fadeOverlapTime <= fadeInTime, "Fade overlap time is larger than fade in time.");
+
+
+				_ = this.StartCoroutine(FadeOutMusic(this._activeMusicSource, (float) fadeOutTime));
+				_ = this.StartCoroutine(FadeInMusic(this._inactiveMusicSource, (float) fadeInTime, fadeInDelay: fadeOutTime - fadeOverlapTime));
+				
+				SetNextMusicSet();
+				this._scheduleEndTime = AudioSettings.dspTime + (fadeOutTime - fadeOverlapTime);
+				this._lastScheduledClip = null;
 			}
 
-			//var cr = this.StartCoroutine(Testje(this._musicSet.clips[this._musicSet.Index++], 1, 1));
-			//this.StopCoroutine(cr);
-		}
+			// Controleer normale schedule.
+			if (AudioSettings.dspTime + ScheduleAheadDuration <= this._scheduleEndTime) { return; }
 
-		private IEnumerator Testje(Clip clip, float delay, float fadeTime) {
-			DateTime moment = DateTime.Parse("10:00");
-			yield return new WaitUntil(() => moment < DateTime.Now);
+			Clip[] GetClips() {
+				switch (this._lastScheduledMusicSetPart) {
+					case MusicSetPart.Intro:
+						return this._musicSet.Intro;
+					case MusicSetPart.Loop:
+						return this._musicSet.Loop;
+					default:
+						throw new Exception("Method must be updated to handle additional cases.");
+				}
+			}
 
+			// Drie situaties:
+			// 1. Geen last scheduled clip.
+			// 2. Huidige iteratie is groter of gelijk aan clip iteraties.
+			// 3. Huidige iteratie is kleiner dan clip iteraties.
+
+			if (this._lastScheduledClip == null) {
+				this._lastScheduledClip = GetClips()[0];
+				this._lastScheduledPlayIteration = 0;
+				this._lastScheduledClipIndex = 0;
+			}
+
+			if (this._lastScheduledPlayIteration >= this._lastScheduledClip.Iterations) {
+				// We moeten naar de volgende clip.
+				this._lastScheduledClipIndex++;
+				if (GetClips().Length <= this._lastScheduledClipIndex) {
+					this._lastScheduledClipIndex = 0;
+					if (this._lastScheduledMusicSetPart == MusicSetPart.Intro) {
+						this._lastScheduledMusicSetPart = MusicSetPart.Loop;
+					}
+				}
+
+				this._lastScheduledClip = GetClips()[this._lastScheduledClipIndex];
+				this._lastScheduledPlayIteration = 0;
+			}
+
+
+			// Hier gaan we uit van situatie 3.
+			// Huidige iteratie is kleiner dan clip iteraties.
+			// Dus de last scheduled clip moet ingepland worden.
+			this._scheduledCoroutines[this._scheduleEndTime] = this.StartCoroutine(ScheduleEnqueueClipAt(this._lastScheduledClip, this._scheduleEndTime));
+			this._lastScheduledPlayIteration++;
 		}
 
 		/// <summary>
@@ -108,18 +146,20 @@ namespace Spellenbakkerij {
 		/// <param name="clip"></param>
 		/// <param name="loop">When true, this clip will loop. If an additional clip is enqueued after this one the loop will be ignored.</param>
 		/// <param name="playAsSoonAsPossible">The clip will start playing "now" with a short delay to allow preparing for playback. The currently playing clip will fade out within this delay time.</param>
-		private void EnqueueMusic(Clip clip) {
-			IsPlaying = true;
+		private IEnumerator ScheduleEnqueueClipAt(Clip clip, double timeToPlay) {
+			Debug.Assert(clip.Duration >= MusicPreparationDelay, "Clip is shorter than preparation delay.");
+			this._scheduleEndTime = Math.Max(timeToPlay + clip.Duration, this._scheduleEndTime);
+
+			yield return new WaitForSecondsRealtime((float) ((timeToPlay - AudioSettings.dspTime) - MusicPreparationDelay));
+			Debug.Log($"AudioSystem.ScheduleEnqueueClipAt(): {AudioSettings.dspTime} > Enqueue \"{clip.audioClip.name}\" at {timeToPlay}");
+			Debug.Log($"AudioSystem.ScheduleEnqueueClipAt(): {AudioSettings.dspTime} > {this._activeMusicSource.GetInstanceID()} > {clip.audioClip.name}, _musicCurrentPlayingEndTime={_clipEndTime}");
 
 			this.ToggleMusicSource();
-			this.ActiveMusicSource.clip = clip.audioClip;
-			this.ActiveMusicSource.loop = clip.loop;
-			Debug.Log($"AudioSystem.EnqueueMusic(): {AudioSettings.dspTime} > {this.ActiveMusicSource.GetInstanceID()} > {clip.audioClip.name}, loop={clip.loop}, _musicSourceIndex={this._musicSourceIndex}, _musicCurrentPlayingEndTime={_musicCurrentPlayingEndTime}");
-			this.ActiveMusicSource.PlayScheduled(this._musicCurrentPlayingEndTime);
+			this._activeMusicSource.clip = clip.audioClip;
+			this._activeMusicSource.PlayScheduled(timeToPlay);
 
-			if (!clip.loop) {
-				this._musicCurrentPlayingEndTime += clip.Duration;
-			}
+			this._clipEndTime = timeToPlay + clip.Duration;
+			_ = this._scheduledCoroutines.TryRemove(timeToPlay, out _);
 		}
 
 		/// <summary>
@@ -138,11 +178,10 @@ namespace Spellenbakkerij {
 			audioSource.Stop();
 		}
 
-		public AudioSource ActiveMusicSource => this._musicSource[_musicSourceIndex];
-		public AudioSource OtherMusicSource => this._musicSource[1 - _musicSourceIndex];
-
 		private void ToggleMusicSource() {
-			this._musicSourceIndex = 1 - this._musicSourceIndex;
+			AudioSource temp = this._activeMusicSource;
+			this._activeMusicSource = this._inactiveMusicSource;
+			this._inactiveMusicSource = temp;
 		}
 
 		private static IEnumerator FadeOutMusic(AudioSource audioSource, float fadeTime) {
@@ -154,11 +193,16 @@ namespace Spellenbakkerij {
 					yield break;
 				}
 			}
+
 			audioSource.Stop();
 			audioSource.volume = startVolume;
 		}
 
-		private static IEnumerator FadeInMusic(AudioSource audioSource, float fadeTime) {
+		private static IEnumerator FadeInMusic(AudioSource audioSource, float fadeTime, double fadeInDelay) {
+			if (fadeInDelay > 0) {
+				yield return new WaitForSecondsRealtime((float) fadeInDelay);
+			}
+
 			float startVolume = audioSource.volume;
 			audioSource.volume = 0;
 			while (audioSource.volume < startVolume) {
@@ -184,5 +228,9 @@ namespace Spellenbakkerij {
 			this.PlaySound(clips[UnityEngine.Random.Range(0, clips.Length)], volume);
 		}
 
+		private enum MusicSetPart {
+			Intro,
+			Loop,
+		}
 	}
 }
